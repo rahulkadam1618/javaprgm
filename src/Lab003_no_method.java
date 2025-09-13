9bbad3cccce7c9b70024e6940e1ecb4f1ff2607d
rahul <rahulkaz28@gmail.com>
2025-09-13 11:09:50 +0530

    newwwww

---
+++ src/Lab003_no_method.java
@@ -0,0 +1,5 @@
+public class Lab003_no_method {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Secondjavaprgm.java
+++ src/Secondjavaprgm.java
@@ -4,6 +4,10 @@
 public class Secondjavaprgm {
 
     public static void main(String[] args) {
+        int age=1500;
+        System.out.println("second......."+ age);
+        System.out.println("second.......");
+        System.out.println("second.......");
         System.out.println("second.......");
 
     }
